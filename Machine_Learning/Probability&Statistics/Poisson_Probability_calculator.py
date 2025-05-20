6756320b01389067d6a9031c7351537aab2664a7
Pulkit <pulkitchauhan414@gmail.com>
2025-05-20 21:58:21 +0530

    Solution

--- Machine_Learning/Probability&Statistics/Poisson_Probability_calculator.py
+++ Machine_Learning/Probability&Statistics/Poisson_Probability_calculator.py
@@ -2,3 +2,10 @@
 in a fixed interval using the Poisson distribution formula.
 The function should take k (number of events) and lam (mean rate of occurrences)
 as inputs and return the probability rounded to 5 decimal places.'''
+
+import math
+
+def poisson_probability(k, lam):
+
+    probability = (math.exp(-lam) * lam**k) / math.factorial(k)
+    return round(probability, 5)
